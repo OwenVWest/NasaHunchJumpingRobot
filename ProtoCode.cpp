9b54e3152616f5c21008f9524c5b0311383e5a4a
OwenVWest <owenvanwestenberg@gmail.com>
2025-10-08 14:50:50 -0600

    math

--- ProtoCode.cpp
+++ ProtoCode.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Wow it works";
+    // Hooke's law (F = -kx)
+    const int k = 58.21; // (N/m)
+    const int xi = 8.89; // (cm)
+    const int A = 6.845; // (cm)
+    int F = 0; // (N)
+    int xf = 0;
+    cout << "Enter the value for var xf: ";
+    cin >> xf;
+    F << (xf-xi);
+    cout << F;
 }
