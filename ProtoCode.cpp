807919fe8a9ffed4f785b4b354498de984f58b18
OwenVWest <owenvanwestenberg@gmail.com>
2025-10-09 12:24:34 -0600

    changed ints to floats

--- ProtoCode.cpp
+++ ProtoCode.cpp
@@ -4,13 +4,15 @@ using namespace std;
 int main()
 {
     // Hooke's law (F = -kx)
-    const int k = 58.21; // (N/m)
-    const int xi = 8.89; // (cm)
-    const int A = 6.845; // (cm)
-    int F = 0; // (N)
-    int xf = 0;
+    const float k = 58.21; // (N/m)
+    const float xi = 8.89; // (cm)
+    const float A = 6.845; // (cm)
+    
+    float F = 0; // (N)
+    float xf = 0;
     cout << "Enter the value for var xf: ";
     cin >> xf;
-    F << (xf-xi);
+    F = xf-xi;
     cout << F;
+
 }
